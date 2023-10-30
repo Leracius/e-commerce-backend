2b5b86e3cf63abb9c5eecf4ea85e8c850f3db140
Axel Quintana <lucasaxelqg15@gmail@email.com>
2023-10-30 01:58:20 -0300

    adding admin options

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -15,12 +15,18 @@ export const getProducts = async(req: Request, res: Response) =>{
 export const createProduct = async(req: Request, res: Response) => {
     console.log('create-product');
     const newProduct: ProductInterface = req.body
+    const adminKey = req.headers["admin-key"]
     
-    const product = new ProductModel(newProduct)
-
-    await product.save()
-    res.status(200).json({
+    if(adminKey === process.env.KEY_FOR_ADMIN){
+        const product = new ProductModel(newProduct)
+        await product.save()
+        res.status(200).json({
         product
-    })
+        });
+    }else{
+        res.status(400).json({
+            msg: "Clave de admin incorrecta"
+        })
+    }
 
 }
