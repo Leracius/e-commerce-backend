0d20c05a36e473531f847b84e5033bab95c184cd
Axel Quintana <lucasaxelqg15@gmail@email.com>
2023-10-30 02:34:30 -0300

    link to documentatio

--- routes/home.ts
+++ routes/home.ts
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
             </head>
             <body>
                 <h1>Hecho por Axel Quintana.</h1>
-                <p>Encuentra la documentación en <a href="https://github.com/Leracius">mi github</a>.</p>
+                <p>Encuentra la documentación en <a href="https://documenter.getpostman.com/view/28688697/2s9YRGypU9">view documentation</a>.</p>
             </body>
         </html>
     `;
